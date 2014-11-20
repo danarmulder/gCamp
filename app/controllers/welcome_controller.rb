566912ec1b1ff9b3813183ce8c212e50033299ae
Dana Mulder <danarmulder@gmail.com>
2014-11-20 13:50:51 -0800

    add array of arrays for quotes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
+    @quotes = [['"Productivity is never an accident.  It is always the result of a commitment to excellence, intelligent planning, and focused effort."', "-Paul J. Meyer",],
+                ['"Your time is limited, so don\'t waste it living somebody else\'s life."', "-Steve Jobs",],
+                ['"Better Ingredients, Better Pizza."', "-Papa John",],
+  ]
   end
 end
