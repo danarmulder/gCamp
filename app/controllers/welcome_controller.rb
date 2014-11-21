4ca14f5caaf682e3deb09e3c9e046c38b1b51d96
Dana Mulder <danarmulder@gmail.com>
2014-11-21 15:00:00 -0800

    quotes fixed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @quotes = [['"Productivity is never an accident.  It is always the result of a commitment to excellence, intelligent planning, and focused effort."', "-Paul J. Meyer",],
+    @quotes = [['"Failure is not an option.  Everyone has to Succeed."', "-Arnold Schwarzenegger",],
                 ['"Your time is limited, so don\'t waste it living somebody else\'s life."', "-Steve Jobs",],
                 ['"Better Ingredients, Better Pizza."', "-Papa John",],
   ]
