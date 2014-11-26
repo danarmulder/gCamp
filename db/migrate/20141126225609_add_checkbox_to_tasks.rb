50f2898fb1132342367616c70d59344d4460858b
Dana Mulder <danarmulder@gmail.com>
2014-11-26 15:38:49 -0800

    add completed to tasks

--- db/migrate/20141126225609_add_checkbox_to_tasks.rb
+++ db/migrate/20141126225609_add_checkbox_to_tasks.rb
@@ -1,4 +1,5 @@
 class AddCheckboxToTasks < ActiveRecord::Migration
   def change
+    add_column :tasks, :complete, :boolean, default: false, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141124222334) do
+ActiveRecord::Schema.define(version: 20141126225609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20141124222334) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "complete",    default: false, null: false
   end
 
 end
