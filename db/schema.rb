84edc469c9d1e250147226792bfc45850b83ecee
Dana Mulder <danarmulder@gmail.com>
2014-12-01 14:17:29 -0800

    add due date to tasks migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141126225609) do
+ActiveRecord::Schema.define(version: 20141201221304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20141126225609) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "complete",    default: false, null: false
+    t.date     "due_date"
   end
 
 end
