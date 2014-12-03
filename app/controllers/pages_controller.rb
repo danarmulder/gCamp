772daadcd687899eca4d47f332fbec7f92d6d15e
Dana Mulder <danarmulder@gmail.com>
2014-12-03 14:53:48 -0800

    change to a FAQ class

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def about
   end
+
+
+  def faq
+    faq1 = Faq.new('What is gCamp?', 'gCamp is an awesome tool that is going to change your life.  gCamp is your one stop shop to organize all your tasks and documents.  You\'ll also be able to track comments that you and others make.  gCamp may eventually replace all need for papers and pens in the world.  Well maybe not but it\'s going to be really cool.')
+    faq2 = Faq.new('How do I join gCamp?', 'Right now, gCamp is still in production.  So there is not a sign up page open to the public yet! Your best option is to become super best friends with a gCamp developer.  They can be found hanging around gSchool during the day and hanging out at the hottest clubs at night.')
+    faq3 = Faq.new('When will gCamp be finished?', 'gCamp is a work in progress.  That being said is should be fully functional by December 2014.  Functional but our developers are going to continue to improve the site for the foreseeable future.  It\'s going to blow your mind.  Organziation is only, (well, will only) be a click away.  Amazing!')
+    @faqs = [faq1, faq2, faq3]
+  end
+
 end
