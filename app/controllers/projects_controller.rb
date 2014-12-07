d86de0b76982832a7289ba6a462c5a1dfdf25f43
Dana Mulder <danarmulder@gmail.com>
2014-12-07 13:12:00 -0800

    add edit and show pages for projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,48 @@ class ProjectsController < ApplicationController
   def index
     @projects = Project.all
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to project_path(@project), notice: "Project was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    @project.update(project_params)
+    if @project.save
+      redirect_to projects_path, notice: "Project was successfully updated."
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path, notice: "Project was successfully deleted."
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(
+      :name
+    )
+  end
 end
