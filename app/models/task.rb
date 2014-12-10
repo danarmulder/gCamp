340163a48b339933e4fff154c36fbd9114c75ea3
Dana Mulder <danarmulder@gmail.com>
2014-12-10 15:52:20 -0800

    add date validaiton to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,13 @@
 class Task < ActiveRecord::Base
 
   validates :description, presence: true
+  validate :date_cannot_be_in_the_past
+
+  def date_cannot_be_in_the_past
+    if due_date.present? && due_date < Date.today
+      errors.add(:date, "can't be in the past")
+    end
+  end
 
   def due_within_week?
     if due_date != nil
