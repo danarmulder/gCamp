bf5980a2517a9e3e548bd710b8226de2b9e4db8c
Dana Mulder <danarmulder@gmail.com>
2014-12-21 22:10:28 -0800

    add validations to memberships

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -3,6 +3,7 @@ class Membership < ActiveRecord::Base
   belongs_to :project
 
   validates :user_id, presence: true
+  validates :project_id, presence: true
 
   validates :user_id, :uniqueness => {:scope => [:project_id]}
 end
